504485afa875863846c09e8be0829839f1cd8ac9
yxz6 <yxzbox@gmail.com>
2022-11-26 00:40:26 -0500

    Add sms

---
+++ sms.go
@@ -0,0 +1,18 @@
+package awskit
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+)
+
+type SMS struct {
+}
+
+func NewSMS(cfg aws.Config) *SMS {
+	return &SMS{}
+}
+
+func (s *SMS) Send(ctx context.Context, recipient []string, text string) error {
+	// TODO:
+	return nil
+}
