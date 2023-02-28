6c88eacbd7461ea52a8852b1356ec8f39f238c20
yxz6 <yxzbox@gmail.com>
2023-02-27 22:16:32 -0500

    fast commit

--- error.go
+++ error.go
@@ -1,9 +1,26 @@
 package aws
 
+import "net/http"
+
 type ErrorString string
 
 func (s ErrorString) Error() string {
 	return string(s)
+}
+
+func (s ErrorString) Code() int {
+	switch s {
+	case ErrInvalidToken:
+		return http.StatusBadRequest
+	case ErrItemNotFound, ErrKeyNotFound:
+		return http.StatusNotFound
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+func (s ErrorString) Status() int {
+	return s.Code()
 }
 
 const (
